98bd5c6c1e2c0da957d1155800ca4900042584cb
apple1417 <theapple1417@gmail.com>
2018-10-08 21:41:59 +1300

    Updated for Randomizer v11.2.0
    mainly added a few new enums

--- apple1417/randomizer_experiments/all_default/one_hub_F6/GeneratorF6.java
+++ apple1417/randomizer_experiments/all_default/one_hub_F6/GeneratorF6.java
@@ -20,7 +20,7 @@ import apple1417.randomizer.TalosProgress;
 
 class GeneratorF6 implements Generator {
     public String getInfo() {
-        return "All default settings, one hub F6, v11.1.0";
+        return "All default settings, one hub F6, v11.2.0";
     }
 
     private static HashMap<String, Integer> TETRO_INDEXES = new HashMap<String, Integer>();
